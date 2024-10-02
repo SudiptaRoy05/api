1121286795dbce94e044648d5cd540b3cb2baa03
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-02 20:58:19 +0600

    async await recap

---
+++ try-catch.js
@@ -0,0 +1,13 @@
+function checkAge(){
+    const age = document.getElementById('age');
+    const ageTxt = age.value;
+    const errTxt = document.getElementById('error');
+    
+    try {
+        console.log(first)    
+    } catch (error) {
+        console.log('ERROR',error)
+        errTxt.innerHTML = 'ERRORR SIRR'
+    }
+    console.log('first')
+}
